db7d37a3679a27477dcaaec1243a01c004b90c05
Joseph Awwal <kwesi1337@yahoo.dk>
2017-08-09 09:48:28 +0200

    CI integration fail

--- test/math-test.py
+++ test/math-test.py
@@ -5,4 +5,4 @@ from src1.math import Math
 class MathTest(unittest.TestCase):
     def test_addition(self):
         #Make test fail
-        self.assertEqual(Math.addition(3,4), 8)
+        self.assertEqual(Math.addition(3,4))
